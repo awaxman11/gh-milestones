cabbf902859f72dbd852d70bbbcc8c26c2759ae7
cjlwired <carlos.jose.lazo@gmail.com>
2013-12-18 23:07:46 -0500

    Refactor configuration options into environment file.

--- app.rb
+++ app.rb
@@ -2,10 +2,6 @@
 require 'bundler'
 Bundler.require
 
-# Include Sinatra libraries.
-require 'sinatra/base'
-require 'sinatra/reloader'
-
 # Include all models in lib/*/ folders.
 require_relative 'environment'
 
@@ -21,19 +17,6 @@ module AppName
     configure do
       set :root, File.dirname(__FILE__)
       set :public_folder, 'public'
-    end
-
-    # ==> Include debug capabilities in development.
-    configure :development do
-      require 'better_errors'
-      require 'binding_of_caller'
-      require 'pry-debugger'
-
-      use BetterErrors::Middleware
-      BetterErrors.application_root = File.expand_path('..', __FILE__)
-
-      register Sinatra::Reloader
-      also_reload 'lib/*/*.rb'
     end
 
     # Database
--- environment.rb
+++ environment.rb
@@ -1,6 +1,23 @@
 # Require additional configuration file if it exists.
 require './config/authentication' if File.exists?('./config/authentication.rb')
 
+# Include Sinatra libraries.
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+# ==> Include debug capabilities in development.
+configure :development do
+  require 'better_errors'
+  require 'binding_of_caller'
+  require 'pry-debugger'
+
+  use BetterErrors::Middleware
+  BetterErrors.application_root = File.expand_path('..', __FILE__)
+
+  register Sinatra::Reloader
+  also_reload 'lib/*/*.rb'
+end
+
 # Include all models (.rb files) in /lib/*/
 Dir.glob('./lib/*').each do |folder|
   Dir.glob(folder +'/*.rb').each do |file|
