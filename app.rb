fc60089ca2e126cd64a174f874809e8b40008bd3
Carlos Lazo <carlos.jose.lazo@gmail.com>
2014-02-11 19:11:59 -0500

    Add database setup within modular application.

--- app.rb
+++ app.rb
@@ -12,6 +12,30 @@ module AppName
   class App < Sinatra::Application
     register Sinatra::ActiveRecordExtension
 
+    # Database Configuration and Setup
+    configure :development do
+     set :database, 'sqlite:///db/development.sqlite3'
+     set :show_exceptions, true
+    end
+
+    configure :test do
+     set :database, 'sqlite:///db/test.sqlite3'
+     set :show_exceptions, true
+    end
+
+    configure :production do
+     db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+
+     ActiveRecord::Base.establish_connection(
+       :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+       :host     => db.host,
+       :username => db.user,
+       :password => db.password,
+       :database => db.path[1..-1],
+       :encoding => 'utf8'
+     )
+    end
+
     # Configure Options
     # => set configuration options.
 
--- environment.rb
+++ environment.rb
@@ -20,29 +20,6 @@ configure :development do
   also_reload 'lib/*/*.rb'
 end
 
-# Database Configuration and Setup
-configure :development do
- set :database, 'sqlite:///db/development.sqlite3'
- set :show_exceptions, true
-end
-
-configure :test do
- set :database, 'sqlite:///db/test.sqlite3'
- set :show_exceptions, true
-end
-
-configure :production do
- db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
-
- ActiveRecord::Base.establish_connection(
-   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-   :host     => db.host,
-   :username => db.user,
-   :password => db.password,
-   :database => db.path[1..-1],
-   :encoding => 'utf8'
- )
-end
 
 # Include all models, concerns, and helpers in /lib/*/*.rb
 Dir.glob('./lib/*').each do |folder|
