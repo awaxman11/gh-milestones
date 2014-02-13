5a196731bdec2b88d4137bdb24e8f78e1f2ef5ae
Carlos Lazo <carlos.jose.lazo@gmail.com>
2014-02-13 01:08:35 -0500

    Fix set location of RACK_ENV variable.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,5 @@
-require File.join(File.expand_path(File.dirname(__FILE__)), '/../app.rb')
-
 ENV['RACK_ENV'] = 'test'
+require File.join(File.expand_path(File.dirname(__FILE__)), '/../app.rb')
 
 require 'sinatra'
 require 'rack/test'
