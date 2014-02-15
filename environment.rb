cbe9bfa73ae23f125f0859ac3e865dd28aa4c5e2
Carlos Lazo <carlos.jose.lazo@gmail.com>
2014-02-15 10:58:00 -0500

    Remove unnecessary logic as PG is only used in production.

--- environment.rb
+++ environment.rb
@@ -27,7 +27,7 @@ configure :test do
 end
 
 configure :production do
- db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+ db = URI.parse(ENV['DATABASE_URL'])
  
  ActiveRecord::Base.establish_connection(
    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
